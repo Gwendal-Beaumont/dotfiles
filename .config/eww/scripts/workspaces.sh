f03989fff8a02885c46e266aceb1b3e5a5a1c9d1
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2025-02-19 09:19:00 +0100

    Cleaner workspace instanciation

--- .config/eww/scripts/workspaces.sh
+++ .config/eww/scripts/workspaces.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-hyprctl workspaces -j | jq -c
+hyprctl workspaces -j | jq -c 'sort_by(.id)'
 
 socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
     hyprctl workspaces -j | jq -c 'sort_by(.id)'
