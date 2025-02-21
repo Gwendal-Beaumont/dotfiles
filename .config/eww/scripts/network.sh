7d78be871e5d36d1853d3e77531c444975506bf1
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2025-02-21 11:59:05 +0100

    New network module with dropdown

--- .config/eww/scripts/network.sh
+++ .config/eww/scripts/network.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-signal=$(nmcli -f in-use,signal dev wifi | rg "\*" | awk '{ print $2 }')
-essid=$(nmcli -t -f NAME connection show --active | head -n1 | sed 's/\"/\\"/g')
-echo '{"essid": "'"$essid"'", "signal": "'"$signal"'"}'
-
-ip monitor link | while read -r line; do
+get_network () {
     signal=$(nmcli -f in-use,signal dev wifi | rg "\*" | awk '{ print $2 }')
     essid=$(nmcli -t -f NAME connection show --active | head -n1 | sed 's/\"/\\"/g')
     echo '{"essid": "'"$essid"'", "signal": "'"$signal"'"}'
+}
+
+get_network
+
+ip monitor link | while read -r line; do
+    get_network
 done
