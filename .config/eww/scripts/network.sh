1ec0f547c2e6cf599c37bab433bf084370bdf3a7
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2025-02-25 15:46:43 +0100

    Adapt network indicator icon to ethernet connections

--- .config/eww/scripts/network.sh
+++ .config/eww/scripts/network.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 get_network () {
-    signal=$(nmcli -f in-use,signal dev wifi | rg "\*" | awk '{ print $2 }')
-    essid=$(nmcli -t -f NAME connection show --active | head -n1 | sed 's/\"/\\"/g')
-    echo '{"essid": "'"$essid"'", "signal": "'"$signal"'"}'
+    signal=$(nmcli -f IN-USE,signal dev wifi | rg "\*" | awk '{ print $2 }')
+    raw=$(nmcli -g NAME,TYPE connection show --active | head -n1)
+    essid=$(echo $raw | awk -F: '{ print $1 }' | sed 's/\"/\\"/g')
+    raw_type=$(echo $raw | awk -F: '{ print $2 }')
+    type=$(if [[ $raw_type == *"ethernet"* ]]; then echo ethernet; else echo wireless; fi)
+    echo '{"essid": "'"$essid"'", "type": "'"$type"'", "signal": "'"$signal"'"}'
 }
 
 get_network
